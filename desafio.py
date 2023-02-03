1544efb49d5db70042cc93a8ca2d4a627c53cb08
Diego Rodrigues <diego17ar@hotmail.com>
2023-02-03 14:44:00 -0300

    correção de variavael

--- desafio.py
+++ desafio.py
@@ -1,7 +1,7 @@
 while True:
     try:
-        num = int(input("Digite um número inteiro: "))
-        if num % 2 == 0:
+        number = int(input("Digite um número inteiro: "))
+        if number % 2 == 0:
             print("O número é par\n")
         else:
             print("O número é ímpar\n")
